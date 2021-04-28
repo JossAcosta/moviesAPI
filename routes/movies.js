2243bc1e482b81c05cc4f3129199bdc8f73e39a2
jossAcosta <josseline@4040apps.com>
2021-04-28 15:34:41 -0500

    cache

--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,8 @@ const {
 
 const validationHandler = require('../utils/middleware/validationHandler');
 
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS }= require('../utils/time');
 function moviesApi(app){
     const router = express.Router();
     app.use("/api/movies", router);
@@ -17,6 +19,7 @@ function moviesApi(app){
     const moviesService = new MoviesService();
 
     router.get("/", async function(req, res, next) {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { tag } = req.query;
         try {
             const movies = await moviesService.getMovies({ tag });
@@ -30,6 +33,7 @@ function moviesApi(app){
         }
     });
     router.get("/:movieId", validationHandler({movieId : movieIdSchema}, 'params'), async function(req, res, next) {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { movieId } = req.params;
         try {
             const movie = await moviesService.getMovie({ movieId });
